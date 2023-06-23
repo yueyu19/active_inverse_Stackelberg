ee2f3a3ef76ec7067519e918070f882d3b09ed59
jake-levy <jake.j.levy@gmail.com>
2023-06-23 17:12:34 -0500

    dyn prop; forgot loops

--- ActiveInverseStackelberg.jl/src/solve.jl
+++ ActiveInverseStackelberg.jl/src/solve.jl
@@ -19,71 +19,97 @@ function solve(;
     u_opt = zeros(ml, tau)      # initialize optimal inputs for leader
     objval_opt = Inf            # initialize optimal value in CCP
 
-    model = Model(Mosek.Optimizer)
-
-    @variables(model, begin
-        ql[1:nl, 1:tau+1]       # leader's costate
-        xl[1:nl, 1:tau+1]       # leader's state
-        w[1:nl]                 # leader's reference state
-        qf[1:nf, 1:tau+1, 1:d]  # hypothesis agent co-state
-        xi[1:nf, 1:tau+1, 1:d]  # hypothesis agent state
-        s[1:Dn]                 # slack variable
-        S[1:tau+1, 1:Dn]        # slack variable
-    end)
-
-    @constraints(model, begin
-        xl[:,1] .== x0              # initial condition for leader's state
-        ql[:,end] .== -cost.Ql*w    # final condition for leader's co-state
-    end)
-
-    for t = 1:tau
-        @constraints(model, begin
-            # leader state dynamics
-            xl[:,t+1] == El[:,:,t]*xl[:,t] - Fl[:,:,t]*ql[:,t+1]
-
-            # leader co-state dynamics
-            ql[:,t] == El[:,:,t]'*ql[:,t+1] - cost.Ql*w
-        end)
-    end
-
-    # reference state constraints
-    for k = 1:d
-        num = (k-1)*n0
-        @constraint(model,
-            #norm(w[num+1:num+2] - x0[num+1:num+2], Inf) <= p.maxrad
-            (w[num+1] - x0[num+1])^2 + (w[num+2] - x0[num+2])^2 <= p.maxrad^2
+    # random initialization of CCP (convex-concave procedure)
+    for ccp_rand = 1:p.maxccpiter 
+        
+        w = x0 + p.maxrad*(2*rand(nl, 1) .- 1)   # random reference point for leader
+        
+        # propagate the leader & follower's states & co-states
+        ql_hat, xl_hat, qf_hat, xi_hat = dynprop(
+            El, Fl, Ef, Ff, cost.Ql, cost.Qf, L, x0, xi0, w
         )
-        @constraint(model, w[num+3:num+4] == [0, 0])
-    end
 
-    for k = 1:d
-        @constraints(model, begin
-            # final condition for k-th follower co-state
-            qf[:,tau+1,k] == -cost.Qf[:,:,k]*L[:,:,k]*xl[:,tau+1]
+        objval_hat = Inf # initialize optimal value for CCP
 
-            # initial condition for k-th follower state
-            xi[:,1,k] == xi0; 
-        end)
+        # CCP iteration
+        for ccp_iter = 1:p.ccp_maxiter 
+            model = Model(Mosek.Optimizer)
 
-        for t = 1:tau
-            @constraints(model, begin
-                # k-th follower's co-state dynamics
-                qf[:,t,k] == Ef[:,:,t,k]'*qf[:,t+1,k] - cost.Qf[:,:,k]*L[:,:,k]*xl[:,t]
-                
-                # k-th follower's state dynamics
-                xi[:,t+1,k] == Ef[:,:,t,k]*xi[:,t,k] - Ff[:,:,t,k]*qf[:,t+1,k]
+            @variables(model, begin
+                ql[1:nl, 1:tau+1]       # leader's costate
+                xl[1:nl, 1:tau+1]       # leader's state
+                w[1:nl]                 # leader's reference state
+                qf[1:nf, 1:tau+1, 1:d]  # hypothesis agent co-state
+                xi[1:nf, 1:tau+1, 1:d]  # hypothesis agent state
+                s[1:Dn]                 # slack variable
+                S[1:tau+1, 1:Dn]        # slack variable
             end)
-        end
-    end
 
-    for ind = 1:Dn
-        for t = 1:tau+1
-            k1 = setD[ind][1]
-            k2 = setD[ind][2]
-            # upper bound quadratics
-            @constraint(model, (xi[:,t,k1] - xi[:,t,k2])'*(Lambdainv[:,:,t,k1]
-                + Lambdainv[:,:,t,k2])*(xi[:,t,k1] - xi[:,t,k2]) <= S[t,ind])
+            @constraints(model, begin
+                xl[:,1] .== x0              # initial condition for leader's state
+                ql[:,end] .== -cost.Ql*w    # final condition for leader's co-state
+            end)
+
+            for t = 1:tau
+                @constraints(model, begin
+                    # leader state dynamics
+                    xl[:,t+1] == El[:,:,t]*xl[:,t] - Fl[:,:,t]*ql[:,t+1]
+
+                    # leader co-state dynamics
+                    ql[:,t] == El[:,:,t]'*ql[:,t+1] - cost.Ql*w
+                end)
+            end
+
+            # reference state constraints
+            for k = 1:d
+                num = (k-1)*n0
+                @constraint(model,
+                    #norm(w[num+1:num+2] - x0[num+1:num+2], Inf) <= p.maxrad
+                    (w[num+1] - x0[num+1])^2 + (w[num+2] - x0[num+2])^2 <= p.maxrad^2
+                )
+                @constraint(model, w[num+3:num+4] == [0, 0])
+            end
+
+            for k = 1:d
+                @constraints(model, begin
+                    # final condition for k-th follower co-state
+                    qf[:,tau+1,k] == -cost.Qf[:,:,k]*L[:,:,k]*xl[:,tau+1]
+
+                    # initial condition for k-th follower state
+                    xi[:,1,k] == xi0; 
+                end)
+
+                for t = 1:tau
+                    @constraints(model, begin
+                        # k-th follower's co-state dynamics
+                        qf[:,t,k] == Ef[:,:,t,k]'*qf[:,t+1,k] - cost.Qf[:,:,k]*L[:,:,k]*xl[:,t]
+                        
+                        # k-th follower's state dynamics
+                        xi[:,t+1,k] == Ef[:,:,t,k]*xi[:,t,k] - Ff[:,:,t,k]*qf[:,t+1,k]
+                    end)
+                end
+            end
+
+            for ind = 1:Dn
+                for t = 1:tau+1
+                    k1 = setD[ind][1]
+                    k2 = setD[ind][2]
+                    # upper bound quadratics
+                    @constraint(model, (xi[:,t,k1] - xi[:,t,k2])'*(Lambdainv[:,:,t,k1]
+                        + Lambdainv[:,:,t,k2])*(xi[:,t,k1] - xi[:,t,k2]) <= S[t,ind])
+                end
+                @constraint(model, sum(sum(S)) - sum(S[:,ind]) == s[ind])
+            end
+
+            objf1 = 0;
+            for t = 1:tau+1
+                for k1 = 1:d-1
+                    for k2 = k1+1:d
+                        objf1 += ((xi_hat[:,t,k1] - xi_hat[:,t,k2])'
+                            *(Lambdainv[:,:,t,k1]+Lambdainv[:,:,t,k2])*(xi[:,t,k1] - xi[:,t,k2]))
+                    end
+                end
+            end
         end
-        @constraint(model, sum(sum(S)) - sum(S[:,ind]) == s[ind])
     end
 end
